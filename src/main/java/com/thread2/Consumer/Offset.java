7710edf211e1afc3e67e27a79802bd43807bba23
cxy <1021618094@qq.com>
2018-04-10 15:44:42 +0800

    modify

--- src/main/java/com/thread2/Consumer/Offset.java
+++ src/main/java/com/thread2/Consumer/Offset.java
@@ -52,42 +52,6 @@ public class Offset {
         return recordsAndOffset;
     }
 
-    public long readFromFile(TopicPartition partition, String filename) {
-        BufferedReader br = null;
-        String str = null;
-        long offset = 0L;
-        File file = new File(Offset.class.getResource("/"+filename).getPath());
-        try {
-            br = new BufferedReader(new FileReader(file));
-            while ((str = br.readLine()) != null) // 判断最后一行不存在，为空结束循环
-            {
-                if (String.valueOf(partition.partition()).equals(str.split(":")[0])) {
-                    offset = Long.parseLong(str.split(":")[1]);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return offset;
-    }
-
-    public void saveToFile(Map<TopicPartition, OffsetAndMetadata> commitMap, String filename) {
-        BufferedWriter Buff = null;
-        File file = new File(Offset.class.getResource("/"+filename).getPath());
-        try {
-            Buff = new BufferedWriter(new FileWriter(file, false));
-            for (TopicPartition partition : commitMap.keySet()) {
-                Buff.write(partition.partition() + ":" + String.valueOf(commitMap.get(partition).offset()));
-                Buff.write("\n");
-            }
-            Buff.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public long getLastCommited(TopicPartition partition) {
         long finalOffset = 0L;
         OffsetAndMetadata offsetAndMetadata = consumer.committed(partition);
@@ -95,7 +59,7 @@ public class Offset {
             finalOffset = offsetAndMetadata.offset();
             System.out.println("上次提交的offset是：" + finalOffset);
         } else {
-            finalOffset = readFromFile(partition, "offset.txt");
+            finalOffset = Utils.readFromFile(partition, "offset.txt");
         }
         return finalOffset;
     }
@@ -119,7 +83,7 @@ public class Offset {
         Map<TopicPartition, OffsetAndMetadata> commitMap = new HashMap<>();
         commitMap.put(partition, commitOffsetAndMetadata);
         consumer.commitSync(commitMap);
-        saveToFile(commitMap, "offset.txt");
+        Utils.saveToFile(commitMap, "offset.txt");
     }
 
 }
--- src/main/java/com/thread2/Consumer/Utils.java
+++ src/main/java/com/thread2/Consumer/Utils.java
@@ -1,6 +1,10 @@
 package com.thread2.Consumer;
 
-import java.io.InputStream;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.io.*;
+import java.util.Map;
 import java.util.Properties;
 
 public class Utils {
@@ -15,5 +19,41 @@ public class Utils {
             e.printStackTrace();
         }
         return props;
+    }
+
+    public static long readFromFile(TopicPartition partition, String filename) {
+        BufferedReader br = null;
+        String str = null;
+        long offset = 0L;
+        File file = new File(Offset.class.getResource("/"+filename).getPath());
+        try {
+            br = new BufferedReader(new FileReader(file));
+            while ((str = br.readLine()) != null) // 判断最后一行不存在，为空结束循环
+            {
+                if (String.valueOf(partition.partition()).equals(str.split(":")[0])) {
+                    offset = Long.parseLong(str.split(":")[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return offset;
+    }
+
+    public static void saveToFile(Map<TopicPartition, OffsetAndMetadata> commitMap, String filename) {
+        BufferedWriter Buff = null;
+        File file = new File(Offset.class.getResource("/"+filename).getPath());
+        try {
+            Buff = new BufferedWriter(new FileWriter(file, false));
+            for (TopicPartition partition : commitMap.keySet()) {
+                Buff.write(partition.partition() + ":" + String.valueOf(commitMap.get(partition).offset()));
+                Buff.write("\n");
+            }
+            Buff.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
